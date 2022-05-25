09c678abf3dbcc5441d3ec1a875f52cb4b6c3c6d
m.giorgi13 <96mat96@libero.it>
2022-05-25 23:12:46 +0200

    used loop in script.sh

--- script.sh
+++ script.sh
@@ -1,38 +1,11 @@
 #!/bin/bash
 
 source venv/Scripts/activate
-python mit_topics.py bc 1990 8
-python mit_topics.py bc 1991 8
-python mit_topics.py bc 1992 8
-python mit_topics.py bc 1993 8
-python mit_topics.py bc 1994 8
-python mit_topics.py bc 1995 8
-python mit_topics.py bc 1996 8
-python mit_topics.py bc 1997 8
-python mit_topics.py bc 1998 8
-python mit_topics.py bc 1999 8
-python mit_topics.py bc 2000 8
-python mit_topics.py bc 2001 8
-python mit_topics.py bc 2002 8
-python mit_topics.py bc 2003 8
-python mit_topics.py bc 2004 8
-python mit_topics.py bc 2005 8
-python mit_topics.py bc 2006 8
-python mit_topics.py bc 2007 8
-python mit_topics.py bc 2008 8
-python mit_topics.py bc 2009 8
-python mit_topics.py bc 2010 8
-python mit_topics.py bc 2011 8
-python mit_topics.py bc 2012 8
-python mit_topics.py bc 2013 8
-python mit_topics.py bc 2014 8
-python mit_topics.py bc 2015 8
-python mit_topics.py bc 2016 8
-python mit_topics.py bc 2017 8
-python mit_topics.py bc 2018 8
-python mit_topics.py bc 2019 8
-python mit_topics.py bc 2020 8
-python mit_topics.py bc 2021 8
-python mit_topics.py bc 2022 8
-echo "DONE"
+
+for i in {1990..2022}
+do
+    python mit_topics.py bc $i 8
+    echo -e "year : '$i'\n"
+done
+echo "COMPLETE"
 sleep 10
