271a84f6b57f01463d4e5f0d08ae02f3789168a9
m.giorgi13 <96mat96@libero.it>
2022-05-25 22:52:00 +0200

    added script

---
+++ script.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+source venv/Scripts/activate
+python mit_topics.py bc 1990 8
+python mit_topics.py bc 1991 8
+python mit_topics.py bc 1992 8
+python mit_topics.py bc 1993 8
+python mit_topics.py bc 1994 8
+python mit_topics.py bc 1995 8
+python mit_topics.py bc 1996 8
+python mit_topics.py bc 1997 8
+python mit_topics.py bc 1998 8
+python mit_topics.py bc 1999 8
+python mit_topics.py bc 2000 8
+python mit_topics.py bc 2001 8
+python mit_topics.py bc 2002 8
+python mit_topics.py bc 2003 8
+python mit_topics.py bc 2004 8
+python mit_topics.py bc 2005 8
+python mit_topics.py bc 2006 8
+python mit_topics.py bc 2007 8
+python mit_topics.py bc 2008 8
+python mit_topics.py bc 2009 8
+python mit_topics.py bc 2010 8
+python mit_topics.py bc 2011 8
+python mit_topics.py bc 2012 8
+python mit_topics.py bc 2013 8
+python mit_topics.py bc 2014 8
+python mit_topics.py bc 2015 8
+python mit_topics.py bc 2016 8
+python mit_topics.py bc 2017 8
+python mit_topics.py bc 2018 8
+python mit_topics.py bc 2019 8
+python mit_topics.py bc 2020 8
+python mit_topics.py bc 2021 8
+python mit_topics.py bc 2022 8
+echo "DONE"
+sleep 10
