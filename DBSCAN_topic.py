00dfdef2ab08470f8262419ff3b549ba501bae99
mattia.didonato95 <mattia.didonato95@gmail.com>
2022-06-04 15:58:13 +0200

    Dpre merge

--- DBSCAN_topic.py
+++ DBSCAN_topic.py
@@ -1,10 +1,16 @@
+import os.path
+
 from sklearn.cluster import DBSCAN
 import numpy as np
+from sklearn.preprocessing import RobustScaler
+from sklearn.metrics.pairwise import cosine_similarity
+
 import PCA_plot3D as pca
 import operator
 
 
 def DBSCAN_Topic(word_vect_dict, year):
+    print("partito dbscan")
     X = []
     for index in range(0, len(word_vect_dict)):
         X.append(list(word_vect_dict.values())[index])
@@ -26,11 +32,6 @@ def DBSCAN_Topic(word_vect_dict, year):
                                       reverse=True)  # clusters ordinati in base al numero di elementi
         number_of_clusters = len(cluster_array_sorted)  # abbiamo trovato il numero di cluster diversi
 
-        # if len(cluster_array_sorted) > 0:
-        #     if cluster_array_sorted[0][0] != -1:
-        #         best_eps[i] = number_of_clusters # numero di cluster diversi
-        #     else:
-
         if cluster_array_sorted[0][0] != -1:
             cluster_array_sorted = cluster_array_sorted[0][0]
         elif len(cluster_array_sorted) == 1:
@@ -44,8 +45,6 @@ def DBSCAN_Topic(word_vect_dict, year):
         for index in range(0, len(word_vect_dict)):
             key.append(clustering.labels_[index])  # prendo gli id dei cluster
             value.append(list(word_vect_dict.values())[index])
-
-       # pca.pca_clustering_3D(value, key, f"/html/year_{year}__radius_{i}")
 
     theBest = sorted(best_eps.items(), key=operator.itemgetter(1), reverse=True)
     clustering = DBSCAN(metric='cosine', eps=theBest[0][0] / 10, min_samples=5).fit(
@@ -64,39 +63,67 @@ def DBSCAN_Topic(word_vect_dict, year):
             dctWord[clustering.labels_[index]].append(list(word_vect_dict.keys())[index])
             dctValue[clustering.labels_[index]].append(list(word_vect_dict.values())[index])
 
-    # for k in sorted(dctWord, key=lambda k: len(dctWord[k]), reverse=True):
-    #     print(k, len(dctWord[k]))
-    #     print(dctWord[k][:50])
-# TODO RIFARE QUESTO METODO
-    with open(f"output/{year}.txt", "w") as f:
-        f.write("selected year: " + year)
-        f.write(" \n")
-        for k in sorted(dctWord, key=lambda k: len(dctWord[k]), reverse=True):
-            f.write("len: " + str(len(dctWord[k])))
-            f.write(" \n")
-            f.write("cluster words:\n")
-            for word in dctWord[k][:50]:
-                f.write(word + ", ")
+    if os.path.exists(f"output/clustering/{year}.txt"):
+        os.remove(f"output/clustering/{year}.txt")
+    bigClusters = {}
+    for g in range(0, len(dctWord)):
+        tot_vectors = {}
+        if len(dctWord[g]) > 50:
+            bigClusters[g] = []
+            for r in range(0, len(dctWord[g])):
+                tot_vectors[dctWord[g][r]] = dctValue[g][r]
+                transformer = RobustScaler(quantile_range=(0, 75.0))  # rimuovo gli outlier
+                transformer.fit(list(tot_vectors.values()))
+                centroid_ = transformer.center_
+                centroid_ = np.array([centroid_])
+                distance_vector = {}
+            for j in range(0, len(tot_vectors) - 1):
+                dist = cosine_similarity(centroid_, np.array([list(tot_vectors.values())[j]]))
+                distance_vector[list(tot_vectors.keys())[j]] = dist[0][0]
+            distance_vector = sorted(distance_vector.items(), key=operator.itemgetter(1), reverse=True)
+            if not os.path.exists(f"output/clustering"):
+                os.makedirs(f"output/clustering")
+                a = "w"
+            else:
+                a = "a"
+            with open(f"output/clustering/{year}.txt", a) as f:
+                f.write("selected year: " + year)
+                f.write(" \n")
+                f.write("len: " + str(len(dctWord[g])))
+                f.write(" \n")
+                f.write("cluster words:\n")
+                for l in range(0, len(distance_vector)):
+                    if l == 100:
+                        break
+                    bigClusters[g].append(distance_vector[l][0])
+                    f.write(distance_vector[l][0] + ", ")
 
-            f.write(" \n")
-            f.write(" \n")
+                f.write(" \n")
+                f.write(" \n")
+        else:
+            if not os.path.exists(f"output/clustering"):
+                os.makedirs(f"output/clustering")
+                a = "w"
+            else:
+                a = "a"
+            with open(f"output/clustering/{year}.txt", a) as f:
+                f.write("selected year: " + year)
+                f.write(" \n")
+                f.write("len: " + str(len(dctWord[g])))
+                f.write(" \n")
+                f.write("cluster words:\n")
+                for l in range(0, len(dctWord[g])):
+                    f.write(dctWord[g][l] + ", ")
 
-    # for index in range(0, len(word_vect_dict)):
-    #     if (c == clustering.labels_[index]):
-    #         key.append(clustering.labels_[index])
-    #         value.append(list(word_vect_dict.values())[index])
-
-    for index in range(0, len(word_vect_dict)):
-        key.append(clustering.labels_[index])
-        value.append(list(word_vect_dict.values())[index])
-
+                f.write(" \n")
+                f.write(" \n")
     key = []
     value = []
     word = []
     for index in range(0, len(word_vect_dict)):
-        if (bestCluster[theBest[0][0]] == clustering.labels_[index]):
-            key.append(clustering.labels_[index])
-            value.append(list(word_vect_dict.values())[index])
-            word.append(list(word_vect_dict.keys())[index])
-
-    return word, value, theBest[0][0]
+        key.append(clustering.labels_[index])
+        value.append(list(word_vect_dict.values())[index])
+        word.append(list(word_vect_dict.keys())[index])
+    pca.pca_clustering_3D(value, key,
+                          f"/output/html/{year}_gtd/year_{year}__radius_{theBest[0][0] / 10}_FinalClustering")
+    return bigClusters
